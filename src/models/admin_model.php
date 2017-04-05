f61a880a346730b3884a7fa90a1aee41b54db77f
owenwestland <13orw@queensu.ca>
2017-04-04 20:09:53 -0400

    Another attempt at formatting the invoice.

--- src/models/admin_model.php
+++ src/models/admin_model.php
@@ -248,27 +248,27 @@
 			}
 
 			$invoice = "Hello ".$user['name'].",\n\tOutlined below is a receipt for your use of the KTCS service:\n\n";
-			$invoice = "".$invoice."\tCar\t\t\t\t\t\t\t\t\tDaily Fee\tReservation Period\t\tCharge\n";
+			$invoice = "".$invoice."\t\t\tDaily Fee\tReservation Period\t\tCharge\t\tCar\n";
 
 			foreach($rh as $h){
+				$invoice = "".$invoice."\t\t\t$".$h->dailyFee."\t\t".$h->reservationLength."\t\t\t\t\t\t$".($h->reservationLength*$h->dailyFee)."\t\t";
 				$carName = $cars[$count]['modelYear']." ".$cars[$count]['make']." ".$cars[$count]['model'];
 				$carName = str_pad($carName, 31);
-				$invoice = "".$invoice."\t".$carName."\t\t";
-				$invoice = "".$invoice.$h->dailyFee."\t\t".$h->reservationLength."\t\t\t\t\t\t".($h->reservationLength*$h->dailyFee)."\n";
+				$invoice = "".$invoice.$carName."\n";
 				$total = $total + $h->reservationLength*$h->dailyFee;
 				$count = $count + 1;
 			}
-			$invoice = "".$invoice."\tTotal:\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t".$total;
+			$invoice = "".$invoice."\tTotal:\t\t\t\t\t\t\t\t\t\t$".$total;
 			$invoice = "".$invoice."\n\nThank you for your continued patronage.\nRegards,\nThe KTCS Team";
 
-			if(!mail($user['email'], "KTCS Invoice", $invoice, $headers)){
-				return "Failed to deliver email. Please check that the user has entered a valid email.";
-			}
+			//if(!mail($user['email'], "KTCS Invoice", $invoice, $headers)){
+			//	return "Failed to deliver email. Please check that the user has entered a valid email.";
+			//}
 
 			//for testing
-			//$file = fopen("temp", 'w');
-			//fwrite($file, $invoice);
-			//fclose($file);
+			$file = fopen("temp", 'w');
+			fwrite($file, $invoice);
+			fclose($file);
 
 			return "Invoice sent successfully.";
 		}
